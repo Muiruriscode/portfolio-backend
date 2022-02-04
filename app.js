0489e9994b907245afb20766d5c1a857e8cb9022
muiruriscode <percymuiruris2@gmail.com>
2022-02-04 16:25:42 +0300

    added cors function

--- app.js
+++ app.js
@@ -22,6 +22,12 @@ const app = express();
 app.use(express.json());
 app.use(errorHandlerMiddleware);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(
   "/api/v1/",
   commentRouter
